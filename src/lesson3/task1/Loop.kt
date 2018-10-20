64250ee2b313217f01c1eade5227b111f70301be
Petrunina Julia <petrunina.yua@edu.spbstu.ru>
2018-10-21 01:14:44 +0300

    Решен 3 урок

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -224,6 +224,7 @@ fun sin(x: Double, eps: Double): Double {
  * cos(x) = 1 - x^2 / 2! + x^4 / 4! - x^6 / 6! + ...
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
  */
+
 fun cos(x: Double, eps: Double): Double {
     val x0 = x % (2 * PI)
     var xNext: Double
